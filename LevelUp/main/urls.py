4289fd47d37aa3338cb45aa2ba38af755dee6c60
NickGalindo <nickdavga@gmail.com>
2021-06-03 20:32:15 -0500

    Changed login and signup and added authorization checks

--- LevelUp/main/urls.py
+++ LevelUp/main/urls.py
@@ -7,7 +7,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('auth/login', views.login, name='login'),
+    path('auth/signup', views.sign_up, name='sign up'),
     path('volume', views.volume, name='volume'),
     path('queue365', views.queue_365, name='queue_365'),
     path('pyramid', views.pyramid, name='pyramid'),
