0d68368434d1b4a332085a4c79d7abd87b2181ca
NickGalindo <nickdavga@gmail.com>
2021-06-05 23:03:15 -0500

    Fixed a bug in queue364 and added data validation

--- LevelUp/LevelUp/apps/trials/views.py
+++ LevelUp/LevelUp/apps/trials/views.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Any
 import subprocess
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from generator.generate import createUsers, createSpecialUsers, specialUsersExist, usersExist
 from generator.queue_365 import queue_workouts
@@ -23,13 +23,15 @@ def queue_365(request : Any):
 
     context = {'title': 'queue_365', 'status':'Initial'}
 
-
-    option = int(input(" \n 0. Array implementation \n 1. References implementation \n"))
-    n = int(input("How many days?: "))
+    try:
+        option = int(input(" \n 0. Array implementation \n 1. References implementation \n"))
+        n = int(input("How many days?: "))
+    except ValueError as e:
+        print("Invalid data")
+        return redirect("/trials/queue365")
 
     if ( option not in [1,0] or n<=0):
         print("Please, check the data!")
-
     else:
         queue_workouts(option, n)
 
@@ -40,7 +42,12 @@ def pyramid(request :Any):
 
     context = {'title': 'pyramidWorkout', 'status':'Initial'}
 
-    n = int(input("how many exercices?"))
+    try:
+        n = int(input("how many exercices?"))
+    except ValueError as e:
+        print("Invalid input")
+        return redirect("/trials/pyramid")
+
     generate_workout(n)
 
     return render(request=request, template_name='basetemplate.html', context=context)
