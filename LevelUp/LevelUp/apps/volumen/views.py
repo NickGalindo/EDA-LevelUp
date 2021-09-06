0acfcbd4508f27ba76dba706a45e426b9536c2e0
Julian ro <davidro2121@gmail.com>
2021-09-05 20:47:46 -0500

    ya tiene las dos implementaciones de hash y muestra los datos, listo para hacer merge con maste

--- LevelUp/LevelUp/apps/volumen/views.py
+++ LevelUp/LevelUp/apps/volumen/views.py
@@ -7,14 +7,37 @@ import random
 import json
 from structures.hash import HashTable
 
+def obtenerVolumen(user :Any):
+    '''
+    funcion para obtener Todos lo volúmenes
+    '''
+    mongoClient = MongoClient('localhost',27017)
+    db = mongoClient['EDA-Project']['user_profiles']
+    arr_workouts = db.find_one({'username': user})['workouts']
+    volumes = []
 
+    for work in arr_workouts:
+        current_vol =0
+        exercises = work["exercises"]
+        
+        for ex in exercises:
+            current_vol += ex['sets'] *ex['reps']*ex['weight']
+
+        volumes.append(current_vol)
+
+    return volumes
 def exercises(n,user):
+     '''
+        funcion para hashear los ejercicios donde llave es el nombre 
+        y valor es el número de veces 
+     '''
+
      mongoClient = MongoClient('localhost',27017)
      db = mongoClient['EDA-Project']['user_profiles']
-     myHashExercises = HashTable(n)
      arr_workouts = db.find_one({'username': user})['workouts']
+
+     myHashExercises = HashTable(n)
      total = 0
-     print(arr_workouts)
 
      for workout in arr_workouts:
          for exercise in workout['exercises']:
@@ -28,60 +51,58 @@ def exercises(n,user):
                  myHashExercises.set(name,current)
              else:
                  myHashExercises.set(name,times)
-         for i in myHashExercises.A:
-             print(i)
 
      return (myHashExercises,total)
 
-
 def cantidadEjercicios(data, hashedExercises,total):
-    #lista de tuplas de ejercicio y su procentaje de veces
+    '''funcion que retorna lista de tuplas (nombre ejercicio, porcentaje de veces con respecto al total)
+        lista de tuplas de ejercicio y su procentaje de veces
+    '''
     l = []
 
     for exercise in data:
         if hashedExercises.hasKey(exercise):
-            porcentaje = round( hashedExercises.get(exercise)/total*100, 2)
-            l.append( (exercise,porcentaje) )
+            porcentaje = round( hashedExercises.get(exercise)/total*100, 2) 
+            l.append( (exercise, porcentaje) )
     
     return l
 
 
-
-
-
 def VolumeHistory(request : Any):
-
 
     mongoClient = MongoClient('localhost',27017)
     db = mongoClient['EDA-Project']['auth_user']
 
     data_file = open("generator/data.json",)
     data = json.load(data_file)
-
-    #arreglo con tuplas (nombre usuario, volúmenenes)
+    #arreglo con tuplas (nombre usuario, volúmenenes[] )
     key_values = []
     #Tabla hash de tamaño n que almacenará las llave/valor 
-    myHash = HashTable(30)
+    myHash = HashTable(2000)
 
-    for user_info in db.find({},{'username':1}):
 
-        # arreglo de los volúmenes generados 
-        volumes = [random.randrange(100)+50 for i in range(10)]
-
-        key_values.append( (user_info['username'],volumes) )
+    #este ciclo calcula los volumenes de los usuarios 
+    for user_info in db.find({"username" : str(request.user)},{'username':1}):
+        
+        volumen_ususario = obtenerVolumen(user_info['username'])
+        key_values.append( (user_info['username'], volumen_ususario) )
     
-    #llenar la tabla hash
+    #Tabla hash que hashea a todos los usuarios de la bd
     for user in key_values:
         myHash.set(user[0],user[1])
 
-    #hashedExercises,total =exercises(15,request.user)
-    hashedExercises,total =exercises(15,"dave herrera")
-    
-    #print(hashedExercises,total)
+    '''
+        segunda implementación de tabla hash
+        almacena en una tabla hash las llaves que son los nombres de los ejercicios
+        y los valores son los números de veces que los ha hecho
+    '''
 
+    hashedExercises,total =exercises(15,str(request.user))
+    
+    #lsita de tuplas (nombre ejercicio, porcentaje veces con respecto al total)
     list_exercices  = cantidadEjercicios(data['random_exercises'],hashedExercises,total)
 
-    print(list_exercices)
+
     context = {'title': 'Volume History', 'volumes': myHash.get(str(request.user)), 'exercises': list_exercices, 'total': total}
     
     return render(request=request, template_name='volume/volume.html', context=context)
